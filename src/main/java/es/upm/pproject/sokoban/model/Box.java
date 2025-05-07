97ab02435c6ddc29e10de9f0ecd8f4055838dbed
elena <elena.gallego@alumnos.upm.es>
2025-05-07 11:15:05 +0200

    Estructura tablero: BOX

--- src/main/java/es/upm/pproject/sokoban/model/Box.java
+++ src/main/java/es/upm/pproject/sokoban/model/Box.java
@@ -2,5 +2,38 @@ package es.upm.pproject.sokoban.model;
 
 public class Box {
 	
+	private int x;
+	private int y;
+	private boolean isOnTarget;
+
+	public Box(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.isOnTarget = false;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public boolean isOnTarget() {
+		return isOnTarget;
+	}
+
+	public void setOnTarget(boolean isOnTarget) {
+		this.isOnTarget = isOnTarget;
+	}
 
 }
