bdd76935d87a9e830b581964af77eef31a25b5b8
elena <elena.gallego@alumnos.upm.es>
2025-06-11 16:45:33 +0200

    MenuView

--- src/main/java/es/upm/pproject/sokoban/view/MenuView.java
+++ src/main/java/es/upm/pproject/sokoban/view/MenuView.java
@@ -1,5 +1,26 @@
 package es.upm.pproject.sokoban.view;
 
-public class MenuView {
+import javax.swing.*;
+import es.upm.pproject.sokoban.controller.GameController;
 
+public class MenuView extends JPanel {
+    public MenuView(GameController gameController) {
+        JButton restartButton = new JButton("Reiniciar");
+        JButton undoButton = new JButton("Deshacer");
+        JButton quitButton = new JButton("Salir");
+
+        restartButton.addActionListener(e -> {
+            gameController.restartLevel();
+        });
+
+        undoButton.addActionListener(e -> {
+            gameController.undoLastMove();
+        });
+
+        quitButton.addActionListener(e -> System.exit(0));
+
+        add(restartButton);
+        add(undoButton);
+        add(quitButton);
+    }
 }
