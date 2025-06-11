4d3f49b602bafb942ff70f5bcdec290dd7c6c242
elena <elena.gallego@alumnos.upm.es>
2025-06-11 16:46:36 +0200

    CellView

--- src/main/java/es/upm/pproject/sokoban/view/CellView.java
+++ src/main/java/es/upm/pproject/sokoban/view/CellView.java
@@ -1,5 +1,41 @@
 package es.upm.pproject.sokoban.view;
+import javax.swing.*;
+import java.awt.*;
+import es.upm.pproject.sokoban.model.Cell;
 
-public class CellView {
+public class CellView extends JPanel {
+    private Cell cell;
 
+    public CellView(Cell cell) {
+        this.cell = cell;
+        setPreferredSize(new Dimension(20, 20));
+        updateAppearance();
+    }
+
+    public void updateCell(Cell newCell) {
+        this.cell = newCell;
+        updateAppearance();
+    }
+
+    private void updateAppearance() {
+        switch (cell.getContent()) {
+            case "Wall":
+                setBackground(Color.DARK_GRAY);
+                break;
+            case "Player":
+                setBackground(Color.BLUE);
+                break;
+            case "Box":
+                setBackground(Color.ORANGE);
+                break;
+            default:
+              if(cell.isTarget()) {
+          setBackground(Color.GREEN);
+        } else {
+          setBackground(new Color(240, 240, 240));
+        }
+                break;
+        }
+        repaint();
+    }
 }
