520c1aed1eb308c960e73886b917aad0290a9009
elena <elena.gallego@alumnos.upm.es>
2025-06-11 16:46:54 +0200

    view

--- src/main/java/es/upm/pproject/sokoban/view/BoardView.java
+++ src/main/java/es/upm/pproject/sokoban/view/BoardView.java
@@ -1,5 +1,32 @@
 package es.upm.pproject.sokoban.view;
+import javax.swing.*;
+import java.awt.*;
+import es.upm.pproject.sokoban.model.Board;
+import es.upm.pproject.sokoban.model.Cell;
 
-public class BoardView {
+public class BoardView extends JPanel {
+    private Board board;
 
+    public BoardView(Board board) {
+        this.board = board;
+        setLayout(new GridLayout(board.getHeight(),board.getWidth()));
+        renderBoard();
+    }
+
+    public void updateBoard(Board newBoard) {
+        this.board = newBoard;
+        renderBoard();
+    }
+
+    private void renderBoard() {
+        removeAll();
+        for (int i = 0; i < board.getHeight(); i++) {
+          for (int j = 0; j < board.getWidth(); j++) {
+            Cell cell = board.getCell(i, j);
+            add(new CellView(cell));
+          }
+        }
+        revalidate();
+        repaint();
+    }
 }
--- src/main/java/es/upm/pproject/sokoban/view/GameView.java
+++ src/main/java/es/upm/pproject/sokoban/view/GameView.java
@@ -1,5 +1,29 @@
 package es.upm.pproject.sokoban.view;
+import java.awt.BorderLayout;
 
-public class GameView {
+import javax.swing.*;
 
+import es.upm.pproject.sokoban.controller.GameController;
+
+public class GameView extends JFrame {
+  private GameController gameController;
+    private BoardView boardView;
+
+    public GameView(GameController gameController) {
+        this.gameController = gameController;
+        setTitle("Sokoban");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800, 600);
+        setLocationRelativeTo(null);
+
+        setLayout(new BorderLayout());
+        add(new MenuView(gameController), BorderLayout.NORTH);
+
+        boardView = new BoardView(gameController.getMovementController().getCurrentBoard());
+        add(boardView, BorderLayout.CENTER);
+    }
+
+    public void updateBoard() {
+        boardView.updateBoard(gameController.getMovementController().getCurrentBoard());
+    }
 }
